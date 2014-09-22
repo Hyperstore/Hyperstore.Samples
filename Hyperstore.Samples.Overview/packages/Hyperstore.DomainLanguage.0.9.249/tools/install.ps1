772075778565cd92b466443cd15273a3d58fc3eb
Alain Metge <alain.metge@hotmail.fr>
2014-09-22 15:43:19 +0200

    First version

--- Hyperstore.Samples.Overview/packages/Hyperstore.DomainLanguage.0.9.249/tools/install.ps1
+++
@@ -1,5 +0,0 @@
-﻿param($installPath, $toolsPath, $package, $project)
-
-$folder = $project.ProjectItems | where-object {$_.Name -eq "Model"}
-$item = $folder.ProjectItems | where-object {$_.Name -eq "Definition.domain"}
-$item.Properties.Item("CustomTool").Value = "MSBuild:Compile"
